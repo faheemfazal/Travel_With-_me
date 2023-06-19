e7c33d8080555b727f4187ba3a6b05cc2bc134e9
Mohammed irfad <muhammedirfad43@gmail.com>
2023-06-19 21:18:20 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
 import Home from './screen/home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+    
+    <Routes>
+  
+  <Route path="/" element={<Home />} />
+  
+ </Routes>
+      
     </div>
   );
 }
